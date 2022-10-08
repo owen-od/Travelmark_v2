06a83eac3ebb34ef25660893533c80186b34b680
Owen O'Donnell <oodonne@tcd.ie>
2022-10-08 15:33:08 +0100

    add recycler view to travelmark list

--- app/src/main/java/ie/wit/travelmark/activities/TravelmarkListActivity.kt
+++ app/src/main/java/ie/wit/travelmark/activities/TravelmarkListActivity.kt
@@ -2,16 +2,56 @@ package ie.wit.travelmark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ie.wit.travelmark.R
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ie.wit.travelmark.databinding.ActivityTravelmarkListBinding
+import ie.wit.travelmark.databinding.CardTravelmarkBinding
 import ie.wit.travelmark.main.MainApp
+import ie.wit.travelmark.models.TravelmarkModel
 
 class TravelmarkListActivity : AppCompatActivity() {
 
     lateinit var app: MainApp
+    private lateinit var binding: ActivityTravelmarkListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_travelmark_list)
+        binding = ActivityTravelmarkListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = TravelmarkAdapter(app.travelmarks)
+    }
+}
+
+class TravelmarkAdapter constructor(private var travelmarks: List<TravelmarkModel>) :
+    RecyclerView.Adapter<TravelmarkAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardTravelmarkBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val travelmark = travelmarks[holder.adapterPosition]
+        holder.bind(travelmark)
+    }
+
+    override fun getItemCount(): Int = travelmarks.size
+
+    class MainHolder(private val binding : CardTravelmarkBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(travelmark: TravelmarkModel) {
+            binding.travelmarkTitle.text = travelmark.title
+            binding.travelmarkDescription.text = travelmark.description
+        }
     }
 }
