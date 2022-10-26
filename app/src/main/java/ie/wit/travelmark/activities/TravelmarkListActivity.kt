9543fde2779f5467fe40b7fc97a56b1800c8a5ca
Owen O'Donnell <oodonne@tcd.ie>
2022-10-26 13:35:26 +0100

    Add searchView to search travelmarks by location

--- app/src/main/java/ie/wit/travelmark/activities/TravelmarkListActivity.kt
+++ app/src/main/java/ie/wit/travelmark/activities/TravelmarkListActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,6 +16,8 @@ import ie.wit.travelmark.adapters.TravelmarkListener
 import ie.wit.travelmark.databinding.ActivityTravelmarkListBinding
 import ie.wit.travelmark.main.MainApp
 import ie.wit.travelmark.models.TravelmarkModel
+import timber.log.Timber
+import timber.log.Timber.i
 
 class TravelmarkListActivity : AppCompatActivity(), TravelmarkListener {
 
@@ -42,7 +46,27 @@ class TravelmarkListActivity : AppCompatActivity(), TravelmarkListener {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
+        // get menu item
+        val searchItem: MenuItem = menu.findItem(R.id.search)
+        // getting search view of menu item and set query hint
+        val searchView: SearchView = searchItem.getActionView() as SearchView
+        searchView.setQueryHint(getString(R.string.search_hint))
+
+        // below line is to call set on query text listener method.
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                filter(query)
+                //i(query)
+                return false
+            }
+
+            override fun onQueryTextChange(msg: String): Boolean {
+                filter(msg)
+                //i(msg)
+                return false
+            }
+        })
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -84,5 +108,23 @@ class TravelmarkListActivity : AppCompatActivity(), TravelmarkListener {
     fun showTravelmarks (travelmarks: List<TravelmarkModel>) {
         binding.recyclerView.adapter = TravelmarkAdapter(travelmarks, this)
         binding.recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    private fun filter(text: String) {
+        val filteredlist: MutableList<TravelmarkModel> = mutableListOf()
+
+        for (item in app.travelmarks.findAll()) {
+            if (item.location.toLowerCase().contains(text.toLowerCase())) {
+                filteredlist.add(item)
+                // i("item added to list")
+            }
+        }
+        if (filteredlist.isEmpty()) {
+            Toast.makeText(this, "No Data Found..", Toast.LENGTH_SHORT).show()
+            loadTravelmarks()
+        } else {
+            binding.recyclerView.adapter = TravelmarkAdapter(filteredlist, this)
+            binding.recyclerView.adapter?.notifyDataSetChanged()
+        }
     }
 }
