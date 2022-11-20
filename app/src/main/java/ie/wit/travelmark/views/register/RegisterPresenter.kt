db4b6566e7c2f01d4f08c47d50d4523d8f1ec715
Owen O'Donnell <oodonne@tcd.ie>
2022-11-20 16:06:27 +0000

    Refactor register activity to use MVP pattern

--- app/src/main/java/ie/wit/travelmark/views/register/RegisterPresenter.kt
+++ app/src/main/java/ie/wit/travelmark/views/register/RegisterPresenter.kt
@@ -1,26 +1,25 @@
-package ie.wit.travelmark.views.login
+package ie.wit.travelmark.views.register
 
 import android.content.Intent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import ie.wit.travelmark.views.register.RegisterView
 import ie.wit.travelmark.main.MainApp
+import ie.wit.travelmark.models.UserModel
 import ie.wit.travelmark.views.travelmarklist.TravelmarkListView
 
-class LoginPresenter(private val view: LoginView) {
+class RegisterPresenter (val view: RegisterView) {
 
     var app: MainApp
     private lateinit var travelmarkIntentLauncher : ActivityResultLauncher<Intent>
-    private lateinit var registerIntentLauncher : ActivityResultLauncher<Intent>
 
-    init{
+    init {
         app = view.application as MainApp
         registerTravelmarkCallback()
-        registerRegisterCallback()
     }
 
-    fun doLogin(username: String, password: String): Boolean {
-        return if (app.users.login(username, password)) {
+    fun doRegisterUser(user: UserModel): Boolean {
+        return if (!user.username.isNullOrBlank() && !user.password.isNullOrBlank()) {
+            app.users.createUser(user)
             val launcherIntent = Intent(view, TravelmarkListView::class.java)
             travelmarkIntentLauncher.launch(launcherIntent)
             true
@@ -29,20 +28,9 @@ class LoginPresenter(private val view: LoginView) {
         }
     }
 
-    fun doRegister() {
-        val launcherIntent = Intent(view, RegisterView::class.java)
-        registerIntentLauncher.launch(launcherIntent)
-    }
-
     private fun registerTravelmarkCallback() {
         travelmarkIntentLauncher =
             view.registerForActivityResult(ActivityResultContracts.StartActivityForResult())
             {  }
     }
-    private fun registerRegisterCallback() {
-        registerIntentLauncher =
-            view.registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-            {  }
-    }
-
 }
--- app/src/main/java/ie/wit/travelmark/views/register/RegisterView.kt
+++ app/src/main/java/ie/wit/travelmark/views/register/RegisterView.kt
@@ -1,33 +1,27 @@
-package ie.wit.travelmark.activities
+package ie.wit.travelmark.views.register
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 import ie.wit.travelmark.databinding.ActivityRegisterBinding
 import ie.wit.travelmark.main.MainApp
 import ie.wit.travelmark.models.UserModel
-import ie.wit.travelmark.views.travelmarklist.TravelmarkListView
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterView : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
-    private lateinit var travelmarkIntentLauncher : ActivityResultLauncher<Intent>
     lateinit var app : MainApp
+    lateinit var presenter: RegisterPresenter
 
     var user = UserModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        registerTravelmarkCallback()
-
         app = application as MainApp
+
+        presenter = RegisterPresenter(this)
 
         binding.registerButton.setOnClickListener {
             user.username = binding.username.text.toString()
@@ -40,13 +34,10 @@ class RegisterActivity : AppCompatActivity() {
                 user.password = ""
                 binding.password.setError("Password must be 3 characters or more")
             }
-            if(!user.username.isNullOrBlank() && !user.password.isNullOrBlank()){
-                app.users.createUser(user.copy())
+            if(presenter.doRegisterUser(user.copy())){
                 Snackbar
                     .make(it, "User Created", Snackbar.LENGTH_LONG)
                     .show()
-                val launcherIntent = Intent(this, TravelmarkListView::class.java)
-                travelmarkIntentLauncher.launch(launcherIntent)
             } else {
                 Snackbar
                     .make(it, "User not created - fill all fields", Snackbar.LENGTH_LONG)
@@ -55,9 +46,4 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    private fun registerTravelmarkCallback() {
-        travelmarkIntentLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-            {  }
-    }
 }
